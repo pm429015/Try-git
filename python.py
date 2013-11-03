d4f3f7e55a7bfdd4adcb6fa3567104b10ede1af2
pm429015 <pm429015@gmail.com>
2013-11-03 10:58:57 -0600

    for 5~9

--- python.py
+++ python.py
@@ -1,28 +1,6 @@
-print "This my first time for git"
-
-print "what happen if I make a change"
-
-print "hellow world"
-
-<<<<<<< HEAD
-print "another change"
-
 
 i = 0
 
 for i in range(5,10):
 	print i
-=======
-print "make a little change in the middle"
-print "another change"
 
-
-print "branch change"
-print "branch very wellcome"
-
-
-i = 0
-while i < 6:
-	print i
-	i+=1
->>>>>>> first-branch
