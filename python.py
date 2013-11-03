b604df812045000c046b3ec13823ce728000170d
pm429015 <pm429015@gmail.com>
2013-11-03 10:58:44 -0600

    whlie 0~5

--- python.py
+++ python.py
@@ -1,16 +1,3 @@
-print "This my first time for git"
-
-print "what happen if I make a change"
-
-print "hellow world"
-
-print "make a little change in the middle"
-print "another change"
-
-
-print "branch change"
-print "branch very wellcome"
-
 
 i = 0
 while i < 6:
